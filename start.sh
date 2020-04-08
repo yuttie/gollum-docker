d8df67bee384303425e0270473bf5d2e34e9748d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-04-08 14:00:46 +0900

    Just use a current working directory as a repository

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 docker run --name=gollum \
            -d \
            --restart=unless-stopped \
-           -v $(pwd)/notes:/wiki \
+           -v $(pwd):/wiki \
            -u $(id -u $USER):$(id -g $USER) \
            -p 4567:8080 \
            gollum
