ec335233f3f60c457cba375d7a910ca240208d6d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-04-09 12:54:00 +0900

    Describe what is done in config.rb

--- config.rb
+++ config.rb
@@ -1,3 +1,4 @@
+# Enable 'tasklist' extension in addition to the default setting
 GitHub::Markup::Markdown::MARKDOWN_GEMS["commonmarker"] = proc { |content, options: {}|
   commonmarker_opts = [:GITHUB_PRE_LANG].concat(options.fetch(:commonmarker_opts, []))
   commonmarker_exts = options.fetch(:commonmarker_exts, [:tagfilter, :autolink, :table, :tasklist, :strikethrough])
